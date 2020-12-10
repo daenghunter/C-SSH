0d70ef10cc9d98380958ceec04d9a8e4f8e7ec82
daenghunter <65704433+daenghunter@users.noreply.github.com>
2020-12-11 02:20:58 +0800

    remove.js

--- remove.js
+++ remove.js
@@ -4,9 +4,9 @@ case '#nobg':
                     var mediaData = await decryptMedia(message, uaOverride)
                     var imageBase64 = `data:${mimetype};base64,${mediaData.toString('base64')}`
                     var base64img = imageBase64
-                    var outFile = './media/img/noBg.png'
+                    var outFile = './media/noBg.png'
                     // untuk api key kalian bisa dapatkan pada website remove.bg
-                    var result = await removeBackgroundFromImageBase64({ base64img, apiKey: 'API-KEY', size: 'auto', type: 'auto', outFile })
+                    var result = await removeBackgroundFromImageBase64({ base64img, apiKey: 'XLFgi9dscDMGXVHa5j6aYDMv', size: 'auto', type: 'auto', outFile })
                     await fs.writeFile(outFile, result.base64img)
                     await tobz.sendImageAsSticker(from, `data:${mimetype};base64,${result.base64img}`)
                 } catch(err) {
