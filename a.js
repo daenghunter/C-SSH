643827b215b8c6b69bb078c3b0d11b9ab663c107
daenghunter <65704433+daenghunter@users.noreply.github.com>
2021-01-08 03:25:37 +0800

    a.js

--- a.js
+++ a.js
@@ -1,1 +1,41 @@
-https://raw.githubusercontent.com/daenghunter/C-SSH/master/scrap.js
+        case '#hayato': {
+
+             const hayato = body.slice(7)
+              if (!hayato) return xbot.reply(from, 'Kirim perintah *#hayato [text]*\n\nContoh : #hayato XBOT', id)
+               xbot.reply(from, mess.wait, id)
+             const puppeteer = require('puppeteer')
+                try {
+                 (async () => {
+             const browser = await puppeteer.launch({
+                  headless: true,
+                                });
+            const page = await browser.newPage();
+               await page
+                    .goto("https://en.ephoto360.com/create-free-fire-facebook-cover-online-567.html", {
+                  waitUntil: "networkidle2"
+                            })
+                .then(async () => {
+                   await page.type("#text-0", hayato);
+                    await page.click("#radio0-radio-1f618436252b4561b3f04d209ec0e1a2");
+                     await new Promise(resolve => setTimeout(resolve, 5000));
+                       await page.click("#submit");
+                       await new Promise(resolve => setTimeout(resolve, 5000));
+              const element = await page.$(
+                                    'div[class="btn-group"] > a'
+                                    );
+                  const text = await (await element.getProperty("href")).jsonValue();
+                                    xbot.sendFileFromUrl(from, text, id)
+                                    browser.close();
+                                                                                
+                                    })
+                                    .catch((err => {
+                                    console.log(err)
+                                    xbot.reply(from, 'error', id)
+                                    }))
+                                    })();
+                                    } catch (error) {
+                                    console.log('error bang')
+                                    xbot.reply(from, 'error', id)
+                                    }
+                                    }
+                                    break
