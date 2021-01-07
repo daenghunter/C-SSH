579e3ddcf2cd6502bc230c6a645e915e9ae064e0
daenghunter <65704433+daenghunter@users.noreply.github.com>
2021-01-08 03:45:34 +0800

    a.js

--- a.js
+++ a.js
@@ -1,41 +1,39 @@
-        case '#pubg': {
-
-             const pubg = body.slice(5)
-              if (!pubg) return xbot.reply(from, 'Kirim perintah *#pubg [text]*\n\nContoh : #pubg XBOT', id)
-               xbot.reply(from, mess.wait, id)
-             const puppeteer = require('puppeteer')
-                try {
-                 (async () => {
-             const browser = await puppeteer.launch({
-                  headless: true,
+case '#matrix': {
+              
+             const logojoker = body.slice(7)
+            const puppeteer = require('puppeteer')
+               if (!logojoker) return geps.reply(from, 'Kirim perintah *#matrix [text]*\n\nContoh : #matrix laylay', id)
+                                geps.reply(from, mess.wait, id)
+                        try {
+                            (async () => {
+                                const browser = await puppeteer.launch({
+                                    headless: true,
                                 });
-            const page = await browser.newPage();
-               await page
-                    .goto("https://en.ephoto360.com/free-pubg-logo-maker-online-609.html", {
-                  waitUntil: "networkidle2"
-                            })
-                .then(async () => {
-                   await page.type("#text-0", pubg);
-                    await page.click("#radio0-radio-c87fef3f338741be9153ad59020d1647");
-                     await new Promise(resolve => setTimeout(resolve, 5000));
-                       await page.click("#submit");
-                       await new Promise(resolve => setTimeout(resolve, 5000));
-              const element = await page.$(
-                                    'div[class="btn-group"] > a'
-                                    );
-                  const text = await (await element.getProperty("href")).jsonValue();
-                                    xbot.sendFileFromUrl(from, text, id)
-                                    browser.close();
-                                                                                
+                                const page = await browser.newPage();
+                                await page
+                                    .goto("https://textpro.me/matrix-style-text-effect-online-884.html", {
+                                        waitUntil: "networkidle2"
+                                    })
+                                    .then(async () => {
+                                        await page.type("#text-0", logojoker);
+                                        await page.click("#submit");
+                                        await new Promise(resolve => setTimeout(resolve, 3000));
+                                        const element = await page.$(
+                                            'div[class="thumbnail"] > img'
+                                            );
+                                        const texts1 = await (await element.getProperty("src")).jsonValue();
+                                        geps.sendFileFromUrl(from, texts1, id)
+                                        browser.close();
+                        
                                     })
                                     .catch((err => {
-                                    console.log(err)
-                                    xbot.reply(from, 'error', id)
+                                        console.log(err)
+                                        geps.reply(from, 'error', id)
                                     }))
-                                    })();
-                                    } catch (error) {
-                                    console.log('error bang')
-                                    xbot.reply(from, 'error', id)
-                                    }
-                                    }
+                            })();
+                        } catch (error) {
+                            console.log('error bang')
+                            geps.reply(from, 'error', id)
+                        }
+                        }
                                     break
