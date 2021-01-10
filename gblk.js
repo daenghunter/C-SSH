a3fc49109bf360a512e192b9047190acbefb87d5
daenghunter <65704433+daenghunter@users.noreply.github.com>
2021-01-10 16:03:08 +0800

    gblk.js

--- gblk.js
+++ gblk.js
@@ -1,47 +1,559 @@
-        case '#spider': {
-            if (!isGroupMsg) return xbot.reply(from, 'Fitur ini hanya bisa di gunakan dalam group', id)
+function help() {
+    return `
+ *Bot Name* : XBOT OFFICIAL 🤖
+ *Version* : Limited Edition
+ *Author* : Alif Putra Darmawan
+ 
+ Berikut daftar perintah yang bisa anda gunakan:
+╭────❉ MENU UTAMA ❉────
+│
+│ Menu ➥ *#ownermenu*
+│ Menu ➥ *#adminmenu*
+│ Menu ➥ *#groupmenu*
+│ Menu ➥ *#nsfwmenu*
+│ Menu ➥ *#mediamenu*
+│ Menu ➥ *#animemenu*
+│ Menu ➥ *#kerangmenu*
+│ Menu ➥ *#downloadmenu*
+│ Menu ➥ *#othermenu*
+│ + Menu ➥ *#limit*
+│ + Menu ➥ *#ping*
+│ + Menu ➥ *#listblock*
+│ + Menu ➥ *#listbanned*
+│ + Menu ➥ *#readme*
+│ + Menu ➥ *#owner*
+│ + Menu ➥ *#donate*
+╰─────────────────
 
-             const spider = body.slice(7)
-              if (!spider) return xbot.reply(from, 'Kirim perintah *#spider [text]*\n\nContoh : #spider XBOT', id)
-               xbot.reply(from, mess.wait, id)
-             const puppeteer = require('puppeteer')
-                try {
-                 (async () => {
-             const browser = await puppeteer.launch({
-                  headless: true,
-                                });
-            const page = await browser.newPage();
-               await page
-                    .goto("https://ephoto360.com/tao-banner-phong-cach-spider-man-465.html", {
-                  waitUntil: "networkidle2"
-                            })
-                .then(async () => {
-                   await page.type("#text-0", spider);
-            
-                    await page.click(" #radio0-radio-hp4feedzl");
-     //                await new Promise(resolve => setTimeout(resolve, 5000));
-                       await page.click("#submit");
-                       await new Promise(resolve => setTimeout(resolve, 10000));
-          await page.waitForSelector(
-            "#link-image"
-          );
-          const element = await page.$(
-         "div.thumbnail > img"
-          );
-          const text = await (await element.getProperty("src")).jsonValue();
-         console.log(text);
-                                    xbot.sendFileFromUrl(from, text, id)
-                                    browser.close();
-                                                                                
-                                    })
-                                    .catch((err => {
-                                    console.log(err)
-                                    xbot.reply(from, 'error', id)
-                                    }))
-                                    })();
-                                    } catch (error) {
-                                    console.log('error bang')
-                                    xbot.reply(from, 'error', id)
-                                    }
-                                    }
-                                    break
+╭────❉ GRATIS❉────
+│ // Fitur akan di update terus
+│ Menu ➥ *#play* Judul Lagu
+│ Menu ➥ *#joox* Judul Lagu
+│ Menu ➥ *#ytmp3* Link Youtube
+│ Menu ➥ *#ytmp4* Link Youtube
+│ Menu ➥ *#sfire* Reply Foto
+│ Menu ➥ *#nulis* Kata² kamu
+│ Menu ➥ *#silkteks* Nama Kamu
+│ Menu ➥ *#tahta* Nama Kamu
+│ Menu ➥ *#bpink* Nama Kamu
+│ Menu ➥ *#partyteks* Nama Kamu
+│ Menu ➥ *#textmaker* Nama Kamu
+│ Menu ➥ *#lovemaker* Nama Kamu
+│ Menu ➥ *#glowmaker* Nama Kamu
+│ Menu ➥ *#fssarah* Nama Kamu
+│ Menu ➥ *#slidteks* |Teks1|Teks2
+│ Menu ➥ *#glitchteks* |Teks1|Teks2
+│ Menu ➥ *#valentinemaker* |Teks1|Teks2
+| Menu ➥ *#googleteks* |Teks1|Teks2|Teks3
+╰───────────────────
+
+╭────❉ Other Menu ❉────
+│ // Fitur akan di update terus
+│ Menu ➥ *#zodiak* [zodiak kamu]
+│ Menu ➥ *#artinama* [nama]
+│ Menu ➥ *#caklontong*
+│ Menu ➥ *#family100*
+│ Menu ➥ *#artimimpi* [nama]
+│ Menu ➥ *#heroml* [nama]
+│ Menu ➥ *#howgay* [nama]
+│ Menu ➥ *#pantun*
+│ Menu ➥ *#fakta*
+│ Menu ➥ *#triggered*
+│ Menu ➥ *#nyimak* Reply Pesan
+╰───────────────────
+
+♻️ Mau pasang iklan di *XBOT OFFICIAL 🤖 ?*
+☎️ WA : *0813-4207-7268*
+  
+⚠️ Gunakan dengan bijak dan jangan melakukan spam terhadap bot ini, Melanggar aturan ? Masuk Daftar *BLOCK*.
+
+*XBOT OFFICIAL 🤖 © 2020*`
+}
+exports.help = help()
+function ownercmd() {
+    return `
+ *Bot Name* : XBOT OFFICIAL 🤖
+ *Version* : Limited Edition
+ *Author* : Alif Putra Darmawan
+ 
+ Berikut daftar perintah yang bisa anda gunakan:
+╭────❉ OWNER BOT ❉────
+│
+│ Menu ➥ *#block 62858xxxxx*
+│ Menu ➥ *#unblock 62858xxxxx*
+│ Menu ➥ *#addadmin @tagmember*
+│ Menu ➥ *#deladmin @tagmember*
+│ Menu ➥ *#restart*
+│ Menu ➥ *#ekickall*
+│ Menu ➥ *#banchat*
+│ Menu ➥ *#unbanchat*
+│ Menu ➥ *#addprem*
+│ Menu ➥ *#delprem*
+│ Menu ➥ *#bc*
+╰─────────────────
+♻️ Mau pasang iklan di *XBOT OFFICIAL 🤖 ?*
+☎️ WA : *0813-4207-7268*
+  
+⚠️ Gunakan dengan bijak dan jangan melakukan spam terhadap bot ini, Melanggar aturan ? Masuk Daftar *BLOCK*.
+
+*XBOT OFFICIAL 🤖 © 2020*`
+}
+exports.ownercmd = ownercmd()
+function admincmd() {
+    return `
+ *Bot Name* : XBOT OFFICIAL 🤖
+ *Version* : Limited Edition
+ *Author* : Alif Putra Darmawan
+ 
+ Berikut daftar perintah yang bisa anda gunakan:
+╭────❉ ADMIN GRUP ❉────
+│
+│ Menu ➥  *#mute*
+│ Menu ➥  *#unmute*
+│ Menu ➥  *#ban @tagmember*
+│ Menu ➥  *#unban @tagmember*
+│ Menu ➥  *#spamcall [81273xxxx]*
+│ Menu ➥  *#kickall*
+│ Menu ➥  *#oleave*
+│ Menu ➥  *#opromote*
+│ Menu ➥  *#odemote*
+│ Menu ➥  *#odelete*
+│ Menu ➥  *#oadd 62813xxxxx*
+│ Menu ➥  *#okickall*
+│ Menu ➥  *#otagall*
+╰──────────────────
+♻️ Mau pasang iklan di *XBOT OFFICIAL 🤖 ?*
+☎️ WA : *0813-4207-7268*
+  
+⚠️ Gunakan dengan bijak dan jangan melakukan spam terhadap bot ini, Melanggar aturan ? Masuk Daftar *BLOCK*.
+
+*XBOT OFFICIAL 🤖 © 2020*`
+}
+exports.admincmd = admincmd()
+function nsfwcmd() {
+    return `
+ *Bot Name* : XBOT OFFICIAL 🤖
+ *Version* : Limited Edition
+ *Author* : Alif Putra Darmawan
+ 
+ Berikut daftar perintah yang bisa anda gunakan: 
+╭────❉ FITUR NSFW ❉────
+│
+│ Menu ➥ *#randomhentai*
+│ Menu ➥ *#randomnsfwneko*
+│ Menu ➥ *#randomtrapnime*
+│ Menu ➥ *#nhentai [kode]*
+│ Menu ➥ *#nhder [kode]*
+│ Menu ➥ *#xnxx [linkXnxx]*
+╰───────────────────
+♻️ Mau pasang iklan di *XBOT OFFICIAL 🤖 ?*
+☎️ WA : *0813-4207-7268*
+  
+⚠️ Gunakan dengan bijak dan jangan melakukan spam terhadap bot ini, Melanggar aturan ? Masuk Daftar *BLOCK*.
+
+*XBOT OFFICIAL 🤖 © 2020*`
+}
+exports.nsfwcmd = nsfwcmd()
+function kerangcmd() {
+    return `
+ *Bot Name* : XBOT OFFICIAL 🤖
+ *Version* : Limited Edition
+ *Author* : Alif Putra Darmawan
+ 
+ Berikut daftar perintah yang bisa anda gunakan:    
+╭────❉ KERANG CMD ❉────
+│
+│ Menu ➥ *#apakah [optional]*
+│ Menu ➥ *#rate* [optional]*
+│ Menu ➥ *#bisakah* [optional]*
+│ Menu ➥ *#kapankah* [optional]*
+╰───────────────────
+♻️ Mau pasang iklan di *XBOT OFFICIAL 🤖 ?*
+☎️ WA : *0813-4207-7268*
+  
+⚠️ Gunakan dengan bijak dan jangan melakukan spam terhadap bot ini, Melanggar aturan ? Masuk Daftar *BLOCK*.
+
+*XBOT OFFICIAL 🤖 © 2020*`
+}
+exports.kerangcmd = kerangcmd()
+function mediacmd() {
+    return `
+ *Bot Name* : XBOT OFFICIAL 🤖
+ *Version* : Limited Edition
+ *Author* : Alif Putra Darmawan
+ 
+ Berikut daftar perintah yang bisa anda gunakan:
+╭────❉ MEDIA CMD ❉────
+│
+│ Menu ➥ *#ramalpasangan [kamu|pasangan]*
+│ Menu ➥ *#artinama [nama]*
+│ Menu ➥ *#covid [negara]*
+│ Menu ➥ *#quotemaker [|teks|author|theme]*
+│ Menu ➥ *#jadwalShalat [daerah]*
+│ Menu ➥ *#jadwalTv [channel]*
+│ Menu ➥ *#cuaca [tempat]*
+│ Menu ➥ *#resepmasakan [optional]*
+│ Menu ➥ *#tts [kode bhs] [teks]*
+│ Menu ➥ *#igstalk [@username]*
+│ Menu ➥ *#tiktokstalk [@username]*
+│ Menu ➥ *#smulestalk [@username]*
+│ Menu ➥ *#kbbi [query]*
+│ Menu ➥ *#wiki [query]*
+│ Menu ➥ *#shopee [query]*
+│ Menu ➥ *#google [query]*
+│ Menu ➥ *#pinterest [query]*
+│ Menu ➥ *#playstore [query]*
+│ Menu ➥ *#googleimage [query]*
+│ Menu ➥ *#jurnalotaku [query]*
+│ Menu ➥ *#brainlysearch [query]*
+│ Menu ➥ *#youtubesearch [query]*
+│ Menu ➥ *#translate [bahasa] [teks]*
+│ Menu ➥ *#brainly [pertanyaan] [.jumlah]*
+│ Menu ➥ *#lirik [optional]*
+│ Menu ➥ *#chord [optional]*
+│ Menu ➥ *#qrcode [optional]*
+│ Menu ➥ *#maps [optional]*
+│ Menu ➥ *#textmaker [teks1|teks2]*
+│ Menu ➥ *#checkip [ipaddress]*
+│ Menu ➥ *#ssweb [linkWeb]*
+│ Menu ➥ *#shorturl [linkWeb]*
+╰────────────────────
+♻️ Mau pasang iklan di *XBOT OFFICIAL 🤖 ?*
+☎️ WA : *0813-4207-7268*
+  
+⚠️ Gunakan dengan bijak dan jangan melakukan spam terhadap bot ini, Melanggar aturan ? Masuk Daftar *BLOCK*.
+
+*XBOT OFFICIAL 🤖 © 2020*`
+}
+exports.mediacmd = mediacmd()
+function animecmd() {
+    return `
+ *Bot Name* : XBOT OFFICIAL 🤖
+ *Version* : Limited Edition
+ *Author* : Alif Putra Darmawan
+ 
+ Berikut daftar perintah yang bisa anda gunakan:
+╭────❉ ANIME CMD ❉────
+│ Menu ➥ *#loli*
+│ Menu ➥ *#shota*
+│ Menu ➥ *#waifu*
+│ Menu ➥ *#husbu*
+│ Menu ➥ *#randomNekoNime*
+│ Menu ➥ *#randomTrapNime*
+│ Menu ➥ *#randomAnime*
+│ Menu ➥ *#quotesnime*
+│ Menu ➥ *#wait*
+│ Menu ➥ *#koin*
+│ Menu ➥ *#malanime [optional]*
+│ Menu ➥ *#malcharacter [optional]*
+│ Menu ➥ *#kusonime [optional]*
+│ Menu ➥ *#otakudesu [optional]*
+│ Menu ➥ *#dewabatch [optional]*
+│ Menu ➥ *#komiku [optional]*
+│ Menu ➥ *#animesearch [query]*
+╰────────────────────
+♻️ Mau pasang iklan di *XBOT OFFICIAL 🤖 ?*
+☎️ WA : *0813-4207-7268*
+  
+⚠️ Gunakan dengan bijak dan jangan melakukan spam terhadap bot ini, Melanggar aturan ? Masuk Daftar *BLOCK*.
+
+*XBOT OFFICIAL 🤖 © 2020*`
+}
+exports.animecmd = animecmd()
+function othercmd() {
+    return `
+ *Bot Name* : XBOT OFFICIAL 🤖
+ *Version* : Limited Edition
+ *Author* : Alif Putra Darmawan
+ 
+ Berikut daftar perintah yang bisa anda gunakan:
+╭────❉ FITUR LAINNYA ❉────
+│ Menu ➥ *#bahasa*
+│ Menu ➥ *#sticker*
+│ Menu ➥ *#stickergif*
+│ Menu ➥ *#stickerfire*
+│ Menu ➥ *#ttp [teks]*
+│ Menu ➥ *#toimg*
+│ Menu ➥ *#neko*
+│ Menu ➥ *#pokemon*
+│ Menu ➥ *#inu*
+│ Menu ➥ *#infoGempa*
+│ Menu ➥ *#quotes*
+│ Menu ➥ *#ptl*
+│ Menu ➥ *#dadu*
+│ Menu ➥ *#koin*
+│ Menu ➥ *#getpp* [Tag Member]
+│ Menu ➥ *#quoterandom*
+╰────────────────────
+♻️ Mau pasang iklan di *XBOT OFFICIAL 🤖 ?*
+☎️ WA : *0813-4207-7268*
+  
+⚠️ Gunakan dengan bijak dan jangan melakukan spam terhadap bot ini, Melanggar aturan ? Masuk Daftar *BLOCK*.
+
+*XBOT OFFICIAL 🤖 © 2020*`
+}
+exports.othercmd = othercmd()
+function downloadcmd() {
+    return `
+ *Bot Name* : XBOT OFFICIAL 🤖
+ *Version* : Limited Edition
+ *Author* : Alif Putra Darmawan
+ 
+ Berikut daftar perintah yang bisa anda gunakan: 
+╭────❉ DOWNLOADER ❉────
+│ Menu ➥ *#ig [linkIg]*
+│ Menu ➥ *#fb [linkFb]*
+│ Menu ➥ *#twitter [linkTwitter]*
+│ Menu ➥ *#smule [linkSmule]*
+│ Menu ➥ *#tiktok [linkTiktok]*
+│ Menu ➥ *#starmaker [linkStarmaker]*
+│ Menu ➥ *#xnxx [linkXnxx]*
+│ Menu ➥ *#nhder [kodeNuclear]*
+│ Menu ➥ *#music [lagu]*
+│ Menu ➥ *#getmusic [IdDownload]*
+│ Menu ➥ *#video [video]*
+│ Menu ➥ *#getvideo [IdDownload]*
+╰────────────────────
+♻️ Mau pasang iklan di *XBOT OFFICIAL 🤖 ?*
+☎️ WA : *0813-4207-7268*
+  
+⚠️ Gunakan dengan bijak dan jangan melakukan spam terhadap bot ini, Melanggar aturan ? Masuk Daftar *BLOCK*.
+
+*XBOT OFFICIAL 🤖 © 2020*`
+}
+exports.downloadcmd = downloadcmd()
+function groupcmd() {
+    return `
+ *Bot Name* : XBOT OFFICIAL 🤖
+ *Version* : Limited Edition
+ *Author* : Alif Putra Darmawan
+ 
+ Berikut daftar perintah yang bisa anda gunakan:  
+╭────❉ GROUP MENU ❉────
+│ Menu ➥ *#snk*
+│ Menu ➥ *#groupinfo*
+│ Menu ➥ *#add 62858xxxxx*
+│ Menu ➥ *#kick @tagmember*
+│ Menu ➥ *#promote @tagmember*
+│ Menu ➥ *#demote @tagadmin*
+│ Menu ➥ *#tagall*
+│ Menu ➥ *#adminList*
+│ Menu ➥ *#ownerGroup*
+│ Menu ➥ *#leave*
+│ Menu ➥ *#delete [replyChatBot]*
+│ Menu ➥ *#kickAll*
+│ Menu ➥ *#NSFW [enable|disable]*
+│ Menu ➥ *#left [enable|disable]*
+│ Menu ➥ *#welcome [enable|disable]*
+│ Menu ➥ *#simi [enable|disable]*
+╰────────────────────
+♻️ Mau pasang iklan di *XBOT OFFICIAL 🤖 ?*
+☎️ WA : *0813-4207-7268*
+  
+⚠️ Gunakan dengan bijak dan jangan melakukan spam terhadap bot ini, Melanggar aturan ? Masuk Daftar *BLOCK*.
+
+*XBOT OFFICIAL 🤖 © 2020*`
+}
+exports.groupcmd = groupcmd()
+
+function readme() {
+    return `
+            *「 STALK 」*
+
+*[@username]* Diisi dengan Username yang valid tanpa tanda “[” dan “]”
+Contoh : *#tiktokstalk @usernane*
+
+*[@username]* Diisi dengan Username yang valid tanpa tanda “[” dan “]”
+Contoh : *#igstalk @username*
+
+*[@username]* Diisi dengan Username yang valid tanpa tanda “[” dan “]”
+Contoh : *#smulestalk @🗡username️*
+
+            *「 YT SEARCH 」*
+
+*[video]* Diisi dengan Judul Video yang valid tanpa tanda “[” dan “]”
+Contoh : *#video Erpan1140*
+Jika ingin mendownload video harap ketik #getvideo [IdDownload] atau #getvideo [urutan]
+
+*[lagu]* Diisi dengan Judul Lagu yang valid tanpa tanda “[” dan “]”
+Contoh : *#music Alan Walker Alone*
+Jika ingin mendownload lagu harap ketik #getmusic [IdDownload] atau #getmusic [urutan]
+
+*[IdDownload] atau [urutan]* Diisi dengan IdDownload yang valid tanpa tanda “[” dan “]”
+Contoh : *Jika tidak reply pesan : #getmusic Iv32bS1*
+Contoh : *Jika reply pesan : #getmusic 1*
+Contoh : *Jika tidak reply pesan : #getvideo Iv32bS1*
+Contoh : *Jika reply pesan : #getvideo 1*
+
+            *「 DOWNLOADER 」*
+
+*[linkStarmaker]* Diisi dengan link Starmaker yang valid tanpa tanda “[” dan “]”
+Contoh : *#starmaker https://m.starmakerstudios.com/d/playrecording?app=sm&from_user_id=3096224747920316&is_convert=true&recordingId=10696049124506354&share_type=copyLink*
+
+*[linkTwitter]* Diisi dengan link YouTube yang valid tanpa tanda “[” dan “]”
+Contoh : *#twitter https://twitter.com/PassengersMovie/status/821025484150423557*
+
+*[linkXnxx]* Diisi dengan link Xnxx yang valid tanpa tanda “[” dan “]”
+Contoh : *#xnxx http://www.xnxx.com/loli/stev-gay*
+
+*[linkNekopoi]* Diisi dengan link Nekopoi yang valid tanpa tanda “[” dan “]”
+Contoh : *#nekopoi https://nekopoi.care/tsunpuri-episode-1-subtitle-indonesia/*
+
+*[linkYt]* Diisi dengan link YouTube yang valid tanpa tanda “[” dan “]”
+Contoh : *#ytmp3 https://youtu.be/Bskehapzke8*
+
+*[linkYt]* Diisi dengan link YouTube yang valid tanpa tanda “[” dan “]”
+Contoh : *#ytmp4 https://youtu.be/Bskehapzke8*
+
+*[linkTiktok]* Diisi dengan link Tiktok yang valid tanpa tanda “[” dan “]”
+Contoh : *#tiktok https://vt.tiktok.com/yqyjPX/*
+
+*[linkSmule]* Diisi dengan link Smule yang valid tanpa tanda “[” dan “]”
+Contoh : *#smule https://www.smule.com/p/767512225_3062360163*
+
+*[linkIg]* Diisi dengan link Instagram yang valid tanpa tanda “[” dan “]”
+Contoh : *#ig https://www.instagram.com/p/CFqRZTlluAi/?igshid=1gtxkbdqhnbbe*
+
+*[linkFb]* Diisi dengan link Facebook yang valid tanpa tanda “[” dan “]”
+Contoh : *#fb https://www.facebook.com/EpochTimesTrending/videos/310155606660409*
+
+*[linkTiktok]* Diisi dengan link facebookt Tiktok yang valid tanpa tanda “[” dan “]”
+Contoh : *#tiktok https://vt.tiktok.com/yqyjPX/*
+
+            *「 OTHER 」*
+
+*[daerah]* Diisi dengan daerah yang valid, tanpa tanda “[” dan “]”
+Contoh : *#jadwalShalat Tangerang*
+
+*[channel]* Diisi dengan channel televisi yang valid, tanpa tanda “[” dan “]”
+Contoh : *#jadwalTv Indosiar*
+
+*[query]* Diisi dengan query/pencarian yang valid, tanpa tanda “[” dan “]“
+Contoh : *#googlesearch siapa itu John Cena️*
+
+*[tempat]* Diisi dengan tempat/lokasi yang valid, tanpa tanda “[” dan “]“
+Contoh : *#cuaca tangerang*
+
+*[kode bhs]* Diisi dengan kode bahasa, contoh *id*, *en*, dll. Dan *[teks]* Diisi dengan teks yang ingin di jadikan voice, Masih sama seperti di atas tanpa tanda “[” dan “]”
+Contoh : *#tts id Test*
+Note : Max 250 huruf
+
+*[|teks|author|theme]* Diisi dengan teks, author, dan theme, tanpa tanda “[” dan “]”
+Contoh : *#quotemaker |Odading|Mang Oleh|Shark*
+
+*[optional]* Diisi dengan teks|title lirik lagu, tanpa tanda “[” dan “]”.
+Contoh : *#lirik aku bukan boneka*
+
+*[ipaddress]* Diisi dengan Ip Address yang valid, tanpa tanda “[” dan “]”.
+Contoh : *#checkip 182.0.144.145*`
+}
+exports.readme = readme()
+function info() {
+    return `
+╭────❉ INFORMATION ❉────
+│ Menu ➥ *Name Bot : XBOT OFFICIAL 🤖️*
+│ Menu ➥ *Version : 1.0*
+│ Menu ➥ *Instagram : @notstrue_*
+╰────────────────────
+  ❏❐❑❒_*XBOT OFFICIAL 🤖*_❏❐❑❒`
+}
+
+exports.info = info()
+function snk() {
+    return `Syarat dan Ketentuan *AZ BOT️*
+1. Teks dan nama pengguna WhatsApp anda akan kami simpan di dalam server selama bot aktif
+2. Data anda akan di hapus ketika bot Offline
+3. Kami tidak menyimpan gambar, video, file, audio, dan dokumen yang anda kirim
+4. Kami tidak akan pernah meminta anda untuk memberikan informasi pribadi
+5. Jika menemukan Bug/Error silahkan langsung lapor ke Owner bot
+6. Apapun yang anda perintah pada bot ini, KAMI TIDAK AKAN BERTANGGUNG JAWAB!
+
+Thanks !`
+}
+exports.snk = snk()
+function sewa() {
+    return `
+Belum mempunyai iklan tertentu.
+
+Jika minat untuk beriklan silahkan contact owner bot :)
+• WA : 081342077268`
+}
+exports.sewa = sewa()
+function sumbang() {
+    return `
+Donasi saat ini silahkan menghubungi owner bot.`
+}
+exports.sumbang = sumbang()
+function listChannel() {
+    return `Daftar channel: 
+1. ANTV
+2. GTV
+3. Indosiar
+4. iNewsTV
+5. KompasTV
+6. MNCTV
+7. METROTV
+8. NETTV
+9. RCTI
+10. SCTV
+11. RTV
+12. Trans7
+13. TransTV`
+}
+exports.listChannel = listChannel()
+function bahasalist() {
+    return `*List kode Bahasa*\n
+  *Code       Bahasa*
+    sq        Albanian
+    ar        Arabic
+    hy        Armenian
+    ca        Catalan
+    zh        Chinese
+    zh-cn     Chinese (China)
+    zh-tw     Chinese (Taiwan)
+    zh-yue    Chinese (Cantonese)
+    hr        Croatian
+    cs        Czech
+    da        Danish
+    nl        Dutch
+    en        English
+    en-au     English (Australia)
+    en-uk     English (United Kingdom)
+    en-us     English (United States)
+    eo        Esperanto
+    fi        Finnish
+    fr        French
+    de        German
+    el        Greek
+    ht        Haitian Creole
+    hi        Hindi
+    hu        Hungarian
+    is        Icelandic
+    id        Indonesian
+    it        Italian
+    ja        Japanese
+    ko        Korean
+    la        Latin
+    lv        Latvian
+    mk        Macedonian
+    no        Norwegian
+    pl        Polish
+    pt        Portuguese
+    pt-br     Portuguese (Brazil)
+    ro        Romanian
+    ru        Russian
+    sr        Serbian
+    sk        Slovak
+    es        Spanish
+    es-es     Spanish (Spain)
+    es-us     Spanish (United States)
+    sw        Swahili
+    sv        Swedish
+    ta        Tamil
+    th        Thai
+    tr        Turkish
+    vi        Vietnamese
+    cy        Welsh
+      `
+}
+exports.bahasalist = bahasalist()
