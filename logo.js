c78abacaf88a0281094e7e38821d1b021662b9cb
daenghunter <65704433+daenghunter@users.noreply.github.com>
2021-01-13 08:46:24 +0800

    logo.js

--- logo.js
+++ logo.js
@@ -1,12 +1,18 @@
+case '#tomp3':
 
-       case '#logo':
-            if (!isGroupMsg) return xbot.reply(from, 'Fitur ini hanya bisa di gunakan dalam group', id)
+if (isMedia || isQuotedVideo){
 
-           // // //  if (!isPremium) return xbot.reply(from, `Perintah ini hanya bisa di gunakan oleh Member Premium!`, id)
-             xbot.reply(from, mess.wait, id)
-             const logo = body.slice(5)
-             if (!logo) return xbot.reply(from, 'Masukin nama mu beb!\nMax 8 huruf biar bagus!', id)
-             if (logo.length > 8) return xbot.reply(from, 'Hurufnya terlalu panjang sayang😔', id)
-             await xbot.sendFileFromUrl(from, `https://api.vhtear.com/gamelogo?text=${logo}&apikey=${vhtearkey}`, `Success-${logo}.jpg`, `Done bosku`, id)
-             limitAdd(serial)
-                break
+if (mimetype === 'video/mp4') {
+
+var medata = await decryptMedia(message, uaOverride)
+
+xbot.reply(from, mess.wait, id)
+
+xbot.sendPtt(from, medata, id)
+
+}
+
+}
+
+break
+
