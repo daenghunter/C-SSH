b5e31c190bbf3a98a35130c10b2706ef3fc013a4
daenghunter <65704433+daenghunter@users.noreply.github.com>
2021-01-15 05:55:02 +0800

    new.js

--- new.js
+++ new.js
@@ -1,56 +1,88 @@
-        case '#puisi': // ARUGAZ
-            if (isLimit(serial)) return xbot.reply(from, `Maaf ${pushname}, Kuota Limit Kamu Sudah Habis, Ketik #limit Untuk Mengecek Kuota Limit Kamu`, id)
-            if (!isGroupMsg) return xbot.reply(from, `Perintah ini hanya bisa di gunakan dalam group!`, id)
-            const puisi = await get.get('https://arugaz.herokuapp.com/api/puisi1').json()
-            xbot.reply(from, `• *Puisi*: ${puisi.data.result}`, id)
-            break
-        case '#puisi2': // ARUGAZ
-            if (isLimit(serial)) return xbot.reply(from, `Maaf ${pushname}, Kuota Limit Kamu Sudah Habis, Ketik #limit Untuk Mengecek Kuota Limit Kamu`, id)
-            const puisi2 = await get.get('https://arugaz.herokuapp.com/api/puisi2').json()
-            xbot.reply(from, `• *Puisi*: ${puisi2.data.result}`, id)
-            break
-        case '#puisi3': // ARUGAZ
-            if (isLimit(serial)) return xbot.reply(from, `Maaf ${pushname}, Kuota Limit Kamu Sudah Habis, Ketik #limit Untuk Mengecek Kuota Limit Kamu`, id)
-            const puisi3 = await get.get('https://arugaz.herokuapp.com/api/puisi3').json()
-            xbot.reply(from, `• *Puisi*: ${puisi3.data.result}`, id)
-            break
-        case '#cersex1': // ARUGAZ
-            if (!isGroupMsg) return xbot.reply(from, `Perintah ini hanya bisa di gunakan dalam group!`, id)
-            if (!isNsfw) return xbot.reply(from, 'command/Perintah NSFW belum di aktifkan di group ini!', id)
-            if (isLimit(serial)) return xbot.reply(from, `Maaf ${pushname}, Kuota Limit Kamu Sudah Habis, Ketik #limit Untuk Mengecek Kuota Limit Kamu`, id)
-            const cersex1 = await get.get('https://arugaz.herokuapp.com/api/cersex1').json()
-            xbot.reply(from, `• *CERSEX*: ${cersex1.data.result.article}`, id)
-            break
-        case '#cersex2': // ARUGAZ
-            if (!isGroupMsg) return xbot.reply(from, `Perintah ini hanya bisa di gunakan dalam group!`, id)
-            if (!isNsfw) return xbot.reply(from, 'command/Perintah NSFW belum di aktifkan di group ini!', id)
-            if (isLimit(serial)) return xbot.reply(from, `Maaf ${pushname}, Kuota Limit Kamu Sudah Habis, Ketik #limit Untuk Mengecek Kuota Limit Kamu`, id)
-            const cersex2 = await get.get('https://arugaz.herokuapp.com/api/cersex1').json()
-            xbot.reply(from, `• *CERSEX*: ${cersex2.data.result.article}`, id)
-            break*/
-        case '#indohot': // ARUGAZ
-            if (!isGroupMsg) return xbot.reply(from, `Perintah ini hanya bisa di gunakan dalam group!`, id)
-         //   if (!isNsfw) return xbot.reply(from, 'command/Perintah NSFW belum di aktifkan di group ini!', id)
-            if (isLimit(serial)) return xbot.reply(from, `Maaf ${pushname}, Kuota Limit Kamu Sudah Habis, Ketik #limit Untuk Mengecek Kuota Limit Kamu`, id)
-            axios.get(`https://arugaz.herokuapp.com/api/indohot`)
-            .then((res) => {
-                let hasil = `Judul = ${res.data.result.judul}\n Genre = ${res.data.result.genre}\n Durasi = ${res.data.result.durasi}\n Link= ${res.data.result.url}`
-                xbot.reply(from, hasil, id)
-            })
-            break
-         case `#mock`:
-            const mock1 = body.slice(6)
-            axios.get('https://arugaz.herokuapp.com/api/bapakfont?kata=' + mock1)
-            .then((res) => {
-                let hasil = `${res.data.result}`
-                xbot.reply(from, hasil, id)
-            })
-            break
-         case `#hilih`:
-            const hilk1 = body.slice(7)
-            axios.get('https://freerestapi.herokuapp.com/api/v1/hilih?kata=' + hilk1)
-            .then((res) => {
-                let hasil = `${res.data.result}`
-                xbot.reply(from, hasil, id)
-            })
-            break
+            case '#kiss':
+                if (!isGroupMsg) return xbot.reply(from, 'Perintah ini hanya bisa di gunakan dalam group!', id)
+                if (isLimit(serial)) return xbot.reply(from, `Maaf ${pushname}, Kuota Limit Kamu Sudah Habis, Ketik #limit Untuk Mengecek Kuota Limit Kamu`, id)
+                 if (args.length === 1) return xbot.reply(from, 'Hanya Owner yang bisa memasukan Bot ke dalam Grup!', id)
+                        const texnugmfq = body.slice(6)
+                       const getnomberr =  await xbot.checkNumberStatus(texnugmfq)
+                    const useriqf = getnomberr.id.replace('@','') + '@c.us'
+                    const jnccck = await xbot.getProfilePicFromServer(useriqf)
+                        const kissqpp = await xbot.getProfilePicFromServer(sender.id)
+                        if (jnccck === undefined) {
+                            var ppFirst = errorImg
+                        } else {
+                            var ppFirst = jnccck
+                        }
+                        canvas.Canvas.kiss(ppFirst, kissqpp)
+                            .then(async (buffer) => {
+                                canvas.write(buffer, `${sender.id}_kiss.png`)
+                                await xbot.sendFile(from, `${sender.id}_kiss.png`, `${sender.id}_kiss.png`, '', id)
+                                fs.unlinkSync(`${sender.id}_kiss.png`)
+                                await limitAdd(serial)
+                            })
+                      break
+				case '#slapp':
+                if (!isGroupMsg) return xbot.reply(from, 'Perintah ini hanya bisa di gunakan dalam group!', id)
+                if (isLimit(serial)) return xbot.reply(from, `Maaf ${pushname}, Kuota Limit Kamu Sudah Habis, Ketik #limit Untuk Mengecek Kuota Limit Kamu`, id)
+                 if (args.length === 1) return xbot.reply(from, 'Hanya Owner yang bisa memasukan Bot ke dalam Grup!', id)
+                        const texnugbmf = body.slice(6)
+                       const getnomberrr =  await xbot.checkNumberStatus(texnugbmf)
+                    const userifb = getnomberrr.id.replace('@','') + '@c.us'
+                    const jncckb = await xbot.getProfilePicFromServer(userifb)
+                        const kissppa = await xbot.getProfilePicFromServer(sender.id)
+                        if (jncckb === undefined) {
+                            var ppFirst = errorImg
+                        } else {
+                            var ppFirst = jncckb
+                        }
+                        canvas.Canvas.batslap(ppFirst, kissppa)
+                            .then(async (buffer) => {
+                                canvas.write(buffer, `${sender.id}_kiss.png`)
+                                await xbot.sendFile(from, `${sender.id}_kiss.png`, `${sender.id}_kiss.png`, '', id)
+                                fs.unlinkSync(`${sender.id}_kiss.png`)
+                                await limitAdd(serial)
+                            })
+                      break
+				case '#spank':
+                if (!isGroupMsg) return xbot.reply(from, 'Perintah ini hanya bisa di gunakan dalam group!', id)
+                if (isLimit(serial)) return xbot.reply(from, `Maaf ${pushname}, Kuota Limit Kamu Sudah Habis, Ketik #limit Untuk Mengecek Kuota Limit Kamu`, id)
+                 if (args.length === 1) return xbot.reply(from, 'Hanya Owner yang bisa memasukan Bot ke dalam Grup!', id)
+                        const texnm = body.slice(6)
+                       const getnomberrrr =  await xbot.checkNumberStatus(texnm)
+                    const userf = getnomberrrr.id.replace('@','') + '@c.us'
+                    const jnccek = await xbot.getProfilePicFromServer(userf)
+                        const kissppt = await xbot.getProfilePicFromServer(sender.id)
+                        if (jnccek === undefined) {
+                            var ppFirst = errorImg
+                        } else {
+                            var ppFirst = jnccek
+                        }
+                        canvas.Canvas.spank(ppFirst, kissppt)
+                            .then(async (buffer) => {
+                                canvas.write(buffer, `${sender.id}_kiss.png`)
+                                await xbot.sendFile(from, `${sender.id}_kiss.png`, `${sender.id}_kiss.png`, '', id)
+                                fs.unlinkSync(`${sender.id}_kiss.png`)
+                                await limitAdd(serial)
+                            })
+                      break
+				case '#distracted':
+                if (!isGroupMsg) return xbot.reply(from, 'Perintah ini hanya bisa di gunakan dalam group!', id)
+                if (isLimit(serial)) return xbot.reply(from, `Maaf ${pushname}, Kuota Limit Kamu Sudah Habis, Ketik #limit Untuk Mengecek Kuota Limit Kamu`, id)
+                 if (args.length === 1) return xbot.reply(from, 'Hanya Owner yang bisa memasukan Bot ke dalam Grup!', id)
+                        const texqnugmf = body.slice(6)
+                       const getnomberrrrr =  await xbot.checkNumberStatus(texqnugmf)
+                    const userife = getnomberrrrr.id.replace('@','') + '@c.us'
+                    const jncckqq = await xbot.getProfilePicFromServer(userife)
+                        const kissppqq = await xbot.getProfilePicFromServer(sender.id)
+                        if (jncckqq === undefined) {
+                            var ppFirst = errorImg
+                        } else {
+                            var ppFirst = jncckqq
+                        }
+                        canvas.Canvas.distracted(ppFirst, kissppqq)
+                            .then(async (buffer) => {
+                                canvas.write(buffer, `${sender.id}_kiss.png`)
+                                await xbot.sendFile(from, `${sender.id}_kiss.png`, `${sender.id}_kiss.png`, '', id)
+                                fs.unlinkSync(`${sender.id}_kiss.png`)
+                                await limitAdd(serial)
+                            })
+                      break
