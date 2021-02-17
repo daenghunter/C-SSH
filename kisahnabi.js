979a828a3203572107c5a4d3e6bda90a31419fe1
daenghunter <65704433+daenghunter@users.noreply.github.com>
2021-02-17 19:49:22 +0800

    kisahnabi.js

--- kisahnabi.js
+++ kisahnabi.js
@@ -1,18 +1,23 @@
-case '/stwm':
-case '#stwm':
-case 'p':
-            const weemnyaboss = body.slice(11)
-            const packname = weemnyaboss.split('|')[0]
-            const author = weemnyaboss.split('|')[1]
-            if (isMedia && type === 'image') {
+case '#stwm': // Bocchi Bot
+         //   if (!isAdmin) return await maxbot.reply(from, `Fitur ini hanya untuk user Premium\n\nKetik #owner\n untuk menyewa bot ini ke grup anda.`, id)
+            if (!q.includes('|')) return await maxbot.reply(from, `Untuk membuat Sticker dengan watermark\nsilahkan upload foto atau reply foto dengan perintah #stickerwm packagename | author\n\nContoh: #stickerwm 2020 | maxbot`, id)
+            if (isMedia && isImage) {
+                await maxbot.reply(from, mess, id)
+	         	const stimker = body.slice(5)
+                const packname = stimker.split('|')[0]
+                const author = stimker.split('|')[1]
                 const mediaData = await decryptMedia(message, uaOverride)
                 const imageBase64 = `data:${mimetype};base64,${mediaData.toString('base64')}`
                 await maxbot.sendImageAsSticker(from, imageBase64, { author: `${author}`, pack: `${packname}` })
-            } else if (quotedMsg && quotedMsg.type == 'image') {
+            } else if (isQuotedImage) {
+                await maxbot.reply(from, mess, id)
+                const stimker = body.slice(5)
+                const packname = stimker.split('|')[0]
+                const author = stimker.split('|')[1]
                 const mediaData = await decryptMedia(quotedMsg, uaOverride)
                 const imageBase64 = `data:${quotedMsg.mimetype};base64,${mediaData.toString('base64')}`
                 await maxbot.sendImageAsSticker(from, imageBase64, { author: `${author}`, pack: `${packname}` })
-            } else {
-                    maxbot.reply(from, mess.error.St, id)
+             } else {
+                await maxbot.reply(from, `Format Yang Anda Masukkan Salah !`, id)
             }
-            break
+    break
